f8952996203536b8283f49e501d900999c39afcf
Mejia1406 <159450913+Mejia1406@users.noreply.github.com>
2024-03-22 20:59:46 -0500

    Create Cultivos.java

---
+++ Cultivos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Cultivos {
+
+  public static void handlePlatano(Scanner scanner) {
+    InfoCultivos.imprimirplatano();
+  }
+
+  public static void handlePotato(Scanner scanner) {
+    InfoCultivos.imprimirpapa();
+  }
+
+  public static void handleCorn(Scanner scanner) {
+    InfoCultivos.imprimirmaiz();
+  }
+
+  public static void handleStrawberry(Scanner scanner) {
+    InfoCultivos.imprimirfresa();
+  }
+
+  public static void handleLettuce(Scanner scanner) {
+    InfoCultivos.imprimirlechuga();
+  }
+}
